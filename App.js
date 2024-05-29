2a81607757dcddba734935ea2318091d162b5225
Lamgs <nghiemkhaclam185.mta@gmail.com>
2024-05-29 17:22:00 +0700

    add stack navigator

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar translucent style='auto'/>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName='SplashScreen'>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
